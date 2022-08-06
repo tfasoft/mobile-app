275efb908e6deba8f697f3fe8391405c59de3f66
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-06 09:35:40 +0430

    Sample field

---
+++ lib/app/widgets/fields/outlined_field.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TFA_OutlinedField extends StatefulWidget {
+  const TFA_OutlinedField({Key? key}) : super(key: key);
+
+  @override
+  State<TFA_OutlinedField> createState() => _TFA_OutlinedFieldState();
+}
+
+class _TFA_OutlinedFieldState extends State<TFA_OutlinedField> {
+  @override
+  Widget build(BuildContext context) {
+    return TextField();
+  }
+}
