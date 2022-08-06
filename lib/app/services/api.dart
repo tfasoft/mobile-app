81ed2094a9f4781f303c06c008cf381e543644a9
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-06 09:35:00 +0430

    Add connect and login api

---
+++ lib/app/services/api.dart
@@ -0,0 +1,44 @@
+import 'package:dio/dio.dart';
+
+class DioClient {
+  final Dio _dio = Dio();
+
+  final _baseUrl = 'http://192.168.1.5:8000/api';
+
+  Future<Response> login(String email, String password) async {
+    Map data = {
+      "email": email,
+      "password": password,
+    };
+
+    Response response;
+
+    try {
+      response = await _dio.post('${_baseUrl}/mobile/login', data: data);
+      
+      return response;
+    } on DioError catch (e) {
+      response = e.response!;
+
+      return response;
+    }
+  }
+
+  Future<Response> connect(String mcode) async {
+    Map data = {
+      "mcode": mcode,
+    };
+
+    Response response;
+
+    try {
+      response = await _dio.post('${_baseUrl}/mobile/connect', data: data);
+
+      return response;
+    } on DioError catch (e) {
+      response = e.response!;
+
+      return response;
+    }
+  }
+}
