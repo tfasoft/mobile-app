3729d6728a4e4f9c1687742a48cd3ac4dacf090d
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-06 11:43:54 +0430

    Add uid, get-uid, set-uid and etc

--- lib/app/services/state.dart
+++ lib/app/services/state.dart
@@ -2,17 +2,23 @@ import 'package:flutter/material.dart';
 
 class AppState extends ChangeNotifier {
   bool _isLogin = false;
+  String _uid = "";
 
   bool get getLogin {
     return _isLogin;
   }
+  String get getUid {
+    return _uid;
+  }
 
-  void login() {
+  void login(String uid) {
     _isLogin = true;
+    _uid = uid;
     notifyListeners();
   }
   void logout() {
     _isLogin = false;
+    _uid = "";
     notifyListeners();
   }
 }
