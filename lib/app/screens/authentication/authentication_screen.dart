f2ad9d819c957f7d9d36c6246f7e1f808cdcebe1
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-06 10:14:06 +0430

    Add login layout

--- lib/app/screens/authentication/authentication_screen.dart
+++ lib/app/screens/authentication/authentication_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:tfasoft_mobile/app/widgets/buttons/full_width_button.dart';
+
+import 'package:tfasoft_mobile/app/widgets/button/tfa_button.dart';
+import 'package:tfasoft_mobile/app/widgets/field/tfa_field.dart';
 
 class AuthenticationScreen extends StatefulWidget {
   const AuthenticationScreen({Key? key}) : super(key: key);
@@ -9,6 +11,68 @@ class AuthenticationScreen extends StatefulWidget {
 }
 
 class _AuthenticationScreenState extends State<AuthenticationScreen> {
+  final TextEditingController _email = TextEditingController();
+  final TextEditingController _password = TextEditingController();
+
+  final TextEditingController _mcode = TextEditingController();
+
+  Future<void> _loginDialog(BuildContext context) async {
+    print("Hi");
+  }
+
+  Future<void> _connectDialog(BuildContext context) async {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Connect to Telegram"),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                TFA_Field(
+                  controller: _mcode,
+                  variant: "outlined",
+                  hint: "Enter M-Code",
+                  label: "M-Code",
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  "If you don't know how to connect your mobile to Telegram, read manual by clicking the manual button.",
+                  style: TextStyle(
+                    fontSize: 12,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TFA_Button(
+              variant: "text",
+              text: "Close",
+              onClick: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TFA_Button(
+              variant: "text",
+              text: "Manual",
+              onClick: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TFA_Button(
+              variant: "contained",
+              text: "Connect",
+              onClick: () {
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,25 +93,40 @@ class _AuthenticationScreenState extends State<AuthenticationScreen> {
                   child: Text(
                     "TFAsoft",
                     style: TextStyle(
-                        color: Colors.blue,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 30),
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                    ),
                   ),
                 ),
                 const SizedBox(height: 30),
-                TFA_FullWidthButton(
-                  text: "Login",
-                  variant: "contained",
-                  onClick: () => print("Hi"),
+                TFA_Field(
+                  controller: _email,
+                  variant: "outlined",
+                  hint: "Enter email",
+                  label: "Email",
                 ),
                 const SizedBox(height: 10),
-                TFA_FullWidthButton(
-                  text: "Register",
+                TFA_Field(
+                  controller: _email,
+                  variant: "outlined",
+                  hint: "Enter password",
+                  label: "Password",
+                ),
+                const SizedBox(height: 10),
+                TFA_Button(
+                  text: "Login",
                   variant: "contained",
-                  onClick: () => print("Hi"),
+                  onClick: () => _loginDialog(context),
+                ),
+                const SizedBox(height: 10),
+                TFA_Button(
+                  text: "Connect to Telegram",
+                  variant: "outlined",
+                  onClick: () => _connectDialog(context),
                 ),
                 const SizedBox(height: 20),
-                TFA_FullWidthButton(
+                TFA_Button(
                   text: "Read manual",
                   variant: "text",
                   onClick: () => {},
