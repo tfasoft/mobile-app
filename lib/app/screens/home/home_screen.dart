b40f56bf9ad6266e0374fb8901f095915da9679d
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-06 10:23:52 +0430

    Add logout

--- lib/app/screens/home/home_screen.dart
+++ lib/app/screens/home/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:tfasoft_mobile/app/services/state.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -15,6 +18,12 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text('Welcome'),
         elevation: 0,
         centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () => Provider.of<AppState>(context, listen: false).logout(),
+            icon: const Icon(Icons.logout),
+          ),
+        ],
       ),
       body: const Center(
         child: Text('Home Screen'),
