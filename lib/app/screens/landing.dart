c6f72774d7aeab5c5e22ab23a25fc19643a0e075
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-06 09:38:18 +0430

    Add watch that login change or not'

--- lib/app/screens/landing.dart
+++ lib/app/screens/landing.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'package:tfasoft_mobile/app/screens/authentication/authentication_screen.dart';
 import 'package:tfasoft_mobile/app/screens/home/home_screen.dart';
+import 'package:tfasoft_mobile/app/services/state.dart';
 
 class Landing extends StatefulWidget {
   const Landing({Key? key}) : super(key: key);
@@ -11,10 +13,9 @@ class Landing extends StatefulWidget {
 }
 
 class _LandingState extends State<Landing> {
-  bool isLogin = true;
-
   @override
   Widget build(BuildContext context) {
+    bool isLogin = Provider.of<AppState>(context, listen: true).getLogin;
     return isLogin ? const HomeScreen() : const AuthenticationScreen();
   }
 }
