fc3fc4492c4b7e3408ba7e97f015d248c30417cf
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-06 09:37:09 +0430

    Add stuff layout

--- lib/app/screens/authentication/authentication_screen.dart
+++ lib/app/screens/authentication/authentication_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tfasoft_mobile/app/widgets/buttons/full_width_button.dart';
 
 class AuthenticationScreen extends StatefulWidget {
   const AuthenticationScreen({Key? key}) : super(key: key);
@@ -16,8 +17,45 @@ class _AuthenticationScreenState extends State<AuthenticationScreen> {
         elevation: 0,
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text('Authentication Screen'),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const Center(
+                  child: Text(
+                    "TFAsoft",
+                    style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                TFA_FullWidthButton(
+                  text: "Login",
+                  variant: "contained",
+                  onClick: () => print("Hi"),
+                ),
+                const SizedBox(height: 10),
+                TFA_FullWidthButton(
+                  text: "Register",
+                  variant: "contained",
+                  onClick: () => print("Hi"),
+                ),
+                const SizedBox(height: 20),
+                TFA_FullWidthButton(
+                  text: "Read manual",
+                  variant: "text",
+                  onClick: () => {},
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
