931da36b1cb9aa34421e65ac5b24aa245d759fa2
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-06 16:29:00 +0430

    Update app satate

--- lib/app/services/state.dart
+++ lib/app/services/state.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class AppState extends ChangeNotifier {
   bool _isLogin = false;
   String _uid = "";
+  Map _user = {};
 
   bool get getLogin {
     return _isLogin;
@@ -10,15 +11,25 @@ class AppState extends ChangeNotifier {
   String get getUid {
     return _uid;
   }
+  Map get getUser {
+    return _user;
+  }
 
-  void login(String uid) {
+  void login(String uid, Map user) {
     _isLogin = true;
     _uid = uid;
+    _user = user;
     notifyListeners();
   }
   void logout() {
     _isLogin = false;
     _uid = "";
+    _user = {};
+    notifyListeners();
+  }
+
+  void setUser(Map user) {
+    _user = user;
     notifyListeners();
   }
 }
