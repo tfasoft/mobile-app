1630b15db21633461cef1faddfe03d10dac072c6
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-07 09:50:33 +0430

    Done in fetch user real time

--- lib/app/screens/landing.dart
+++ lib/app/screens/landing.dart
@@ -1,8 +1,12 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'package:tfasoft_mobile/app/screens/authentication/authentication_screen.dart';
 import 'package:tfasoft_mobile/app/screens/home/home_screen.dart';
+
+import 'package:tfasoft_mobile/app/services/api.dart';
 import 'package:tfasoft_mobile/app/services/state.dart';
 
 class Landing extends StatefulWidget {
@@ -13,9 +17,24 @@ class Landing extends StatefulWidget {
 }
 
 class _LandingState extends State<Landing> {
+  final DioClient _client = DioClient();
+
+  Future<void> getUser(BuildContext context, String uid) async {
+    var response = _client.getUser(uid);
+
+    response.then((result) {
+      if (result.statusCode == 200) {
+        Provider.of<AppState>(context, listen: false).setUser(result.data);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     bool isLogin = Provider.of<AppState>(context, listen: true).getLogin;
+
+    if (isLogin) getUser(context, Provider.of<AppState>(context, listen: true).getUid);
+
     return isLogin ? const HomeScreen() : const AuthenticationScreen();
   }
 }
