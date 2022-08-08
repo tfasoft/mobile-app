a21e604f79f250a46a8e6fd0412ff53087c91414
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-08 11:21:28 +0430

    Add page title

---
+++ lib/app/widgets/title/page_title.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class UnderAppBar extends StatefulWidget {
+  const UnderAppBar({
+    Key? key,
+    this.title,
+  }) : super(key: key);
+
+  final title;
+
+  @override
+  State<UnderAppBar> createState() => _UnderAppBarState();
+}
+
+class _UnderAppBarState extends State<UnderAppBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Text(
+          widget.title,
+          style: const TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            color: Colors.blueGrey,
+          ),
+        ),
+        const SizedBox(height: 20),
+      ],
+    );
+  }
+}
