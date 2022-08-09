1bbc8552084999a75b6affe0c76cdacd1a16773d
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-09 18:55:47 +0430

    Add change name

--- lib/app/screens/home/settings/profile_settings.dart
+++ lib/app/screens/home/settings/profile_settings.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:tfasoft_mobile/app/services/api.dart';
+import 'package:tfasoft_mobile/app/services/state.dart';
+import 'package:tfasoft_mobile/app/widgets/button/tfa_button.dart';
+import 'package:tfasoft_mobile/app/widgets/field/tfa_field.dart';
+import 'package:tfasoft_mobile/app/widgets/title/page_subtitle.dart';
 
 class ProfileSettings extends StatefulWidget {
   const ProfileSettings({Key? key}) : super(key: key);
@@ -8,6 +15,8 @@ class ProfileSettings extends StatefulWidget {
 }
 
 class _ProfileSettingsState extends State<ProfileSettings> {
+  final DioClient _client = DioClient();
+
   Future<void> _showSnackBar(BuildContext context, String message) async {
     final snackBar = SnackBar(
       content: Text(message),
@@ -21,6 +30,29 @@ class _ProfileSettingsState extends State<ProfileSettings> {
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 
+  final TextEditingController _name = TextEditingController();
+
+  bool loadingName = false;
+
+  Future<void> changeName(BuildContext context) async {
+    setState(() => loadingName = true);
+
+    String uid = Provider.of<AppState>(context, listen: false).getUid;
+    Map data = {
+      "name": _name.text,
+    };
+
+    var response = _client.changeUserData(uid, data);
+
+    response.then((result) {
+      setState(() => loadingName = false);
+
+      if (result.statusCode == 200) {
+        _showSnackBar(context, "Name updated successfully");
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,7 +64,29 @@ class _ProfileSettingsState extends State<ProfileSettings> {
         title: const Text("Profile Settings"),
         elevation: 0,
       ),
-      body: const Center(child: Text("Coming soon", style: TextStyle(color: Colors.blueGrey))),
-    );;
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const PageSubTitle(title: 'Change name'),
+            TFA_Field(
+              variant: "outlined",
+              password: false,
+              label: "Name",
+              hint: "Enter your new namw",
+              controller: _name,
+            ),
+            const SizedBox(height: 10),
+            TFA_Button(
+              variant: "contained",
+              onClick: loadingName ? null : () => changeName(context),
+              text: "Update name",
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
